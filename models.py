9efaed55049fb65d6b8fbb56fe002318ea92671f
mrzhu <mrzhu.cool@gmail.com>
2017-03-17 22:47:50 +0800

    Modify models.py
    Modify the structure of D.
    Modify the annotation.

--- models.py
+++ models.py
@@ -51,48 +51,48 @@ class G(nn.Module):
         # Convolution layers:
         # input is (nc) x 256 x 256
         e1 = self.conv1(input)
-        # input is (ngf) x 128 x 128
+        # state size is (ngf) x 128 x 128
         e2 = self.batch_norm2(self.conv2(self.leaky_relu(e1)))
-        # input is (ngf x 2) x 64 x 64
+        # state size is (ngf x 2) x 64 x 64
         e3 = self.batch_norm4(self.conv3(self.leaky_relu(e2)))
-        # input is (ngf x 4) x 32 x 32
+        # state size is (ngf x 4) x 32 x 32
         e4 = self.batch_norm8(self.conv4(self.leaky_relu(e3)))
-        # input is (ngf x 8) x 16 x 16
+        # state size is (ngf x 8) x 16 x 16
         e5 = self.batch_norm8(self.conv5(self.leaky_relu(e4)))
-        # input is (ngf x 8) x 8 x 8
+        # state size is (ngf x 8) x 8 x 8
         e6 = self.batch_norm8(self.conv6(self.leaky_relu(e5)))
-        # input is (ngf x 8) x 4 x 4
+        # state size is (ngf x 8) x 4 x 4
         e7 = self.batch_norm8(self.conv7(self.leaky_relu(e6)))
-        # input is (ngf x 8) x 2 x 2
+        # state size is (ngf x 8) x 2 x 2
         # No batch norm on output of Encoder
         e8 = self.conv8(self.leaky_relu(e7))
 
         # Decoder
         # Deconvolution layers:
-        # input is (ngf x 8) x 1 x 1
+        # state size is (ngf x 8) x 1 x 1
         d1_ = self.dropout(self.batch_norm8(self.dconv1(self.relu(e8))))
-        # input is (ngf x 8) x 2 x 2
+        # state size is (ngf x 8) x 2 x 2
         d1 = torch.cat((d1_, e7), 1)
         d2_ = self.dropout(self.batch_norm8(self.dconv2(self.relu(d1))))
-        # input is (ngf x 8) x 4 x 4
+        # state size is (ngf x 8) x 4 x 4
         d2 = torch.cat((d2_, e6), 1)
         d3_ = self.dropout(self.batch_norm8(self.dconv3(self.relu(d2))))
-        # input is (ngf x 8) x 8 x 8
+        # state size is (ngf x 8) x 8 x 8
         d3 = torch.cat((d3_, e5), 1)
         d4_ = self.batch_norm8(self.dconv4(self.relu(d3)))
-        # input is (ngf x 8) x 16 x 16
+        # state size is (ngf x 8) x 16 x 16
         d4 = torch.cat((d4_, e4), 1)
         d5_ = self.batch_norm4(self.dconv5(self.relu(d4)))
-        # input is (ngf x 4) x 32 x 32
+        # state size is (ngf x 4) x 32 x 32
         d5 = torch.cat((d5_, e3), 1)
         d6_ = self.batch_norm2(self.dconv6(self.relu(d5)))
-        # input is (ngf x 2) x 64 x 64
+        # state size is (ngf x 2) x 64 x 64
         d6 = torch.cat((d6_, e2), 1)
         d7_ = self.batch_norm(self.dconv7(self.relu(d6)))
-        # input is (ngf) x 128 x 128
+        # state size is (ngf) x 128 x 128
         d7 = torch.cat((d7_, e1), 1)
         d8 = self.dconv8(self.relu(d7))
-        # input is (nc) x 256 x 256
+        # state size is (nc) x 256 x 256
         output = self.tanh(d8)
         return output
 
@@ -102,11 +102,8 @@ class D(nn.Module):
         self.conv1 = nn.Conv2d(input_nc + output_nc, ndf, 4, 2, 1)
         self.conv2 = nn.Conv2d(ndf, ndf * 2, 4, 2, 1)
         self.conv3 = nn.Conv2d(ndf * 2, ndf * 4, 4, 2, 1)
-        self.conv4 = nn.Conv2d(ndf * 4, ndf * 8, 4, 2, 1)
-        self.conv5 = nn.Conv2d(ndf * 8, ndf * 8, 4, 2, 1)
-        self.conv6 = nn.Conv2d(ndf * 8, ndf * 8, 4, 2, 1)
-        self.conv7 = nn.Conv2d(ndf * 8, ndf * 8, 4, 1, 1)
-        self.conv8 = nn.Conv2d(ndf * 8, 1, 4, 1, 1)
+        self.conv4 = nn.Conv2d(ndf * 4, ndf * 8, 4, 1, 1)
+        self.conv5 = nn.Conv2d(ndf * 8, 1, 4, 1, 1)
 
         self.batch_norm2 = nn.BatchNorm2d(ndf * 2)
         self.batch_norm4 = nn.BatchNorm2d(ndf * 4)
@@ -119,12 +116,14 @@ class D(nn.Module):
     def forward(self, input):
         # input is (nc x 2) x 256 x 256
         h1 = self.conv1(input)
+        # state size is (ndf) x 128 x 128
         h2 = self.batch_norm2(self.conv2(self.leaky_relu(h1)))
+        # state size is (ndf x 2) x 64 x 64
         h3 = self.batch_norm4(self.conv3(self.leaky_relu(h2)))
+        # state size is (ndf x 4) x 32 x 32
         h4 = self.batch_norm8(self.conv4(self.leaky_relu(h3)))
-        h5 = self.batch_norm8(self.conv5(self.leaky_relu(h4)))
-        h6 = self.batch_norm8(self.conv6(self.leaky_relu(h5)))
-        h7 = self.batch_norm8(self.conv7(self.leaky_relu(h6)))
-        h8 = self.conv8(h7)
-        output = self.sigmoid(h8)
+        # state size is (ndf x 8) x 31 x 31
+        h5 = self.conv5(self.leaky_relu(h4))
+        # state size is (ndf) x 30 x 30, corresponds to 70 x 70 receptive
+        output = self.sigmoid(h5)
         return output
